c740573bcf9d2c98cdf91242ba811432ee67050a
dmkuehn6 <dmkuehn6@gmail.com>
2018-07-12 20:32:27 -0400

    Revamped UI with navbar

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => res.render('index'));
-routes.get('/developer', (req, res) => res.render('developer'))
-routes.get('/photographer', (req, res) => res.render('photographer'))
+routes.get('/software', (req, res) => res.render('software'))
+routes.get('/photography', (req, res) => res.render('photography'))
 
 module.exports = routes;
