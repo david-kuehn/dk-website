9c29c4e4aa5152eaff9b5910eac1287c34c5a502
dmkuehn6 <dmkuehn6@gmail.com>
2018-07-22 14:54:49 -0400

    Added top photo box that pulls from 4 random images. Also added a tagline. Need to style the elements.

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => res.render('index'));
+
+
 routes.get('/software', (req, res) => res.render('software'))
 routes.get('/photography', (req, res) => res.render('photography'))
 
