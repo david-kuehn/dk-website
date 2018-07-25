581073e2d7cc44df6a02120d5e3acef35dc29be3
David Kuehn <dmkuehn6@gmail.com>
2018-07-24 19:13:21 -0500

    Added dynamic navbar that changes colors. Still need to fix height adjustment.

---
+++ public/scripts/navbar.js
@@ -0,0 +1,26 @@
+$(document).ready(configureNavbar())
+
+function configureNavbar () {
+  // When the window is scrolled
+  $(window).scroll(function () {
+    // If the window has been scrolled more than 50px
+    if ($(this).scrollTop() > 50) {
+      // Assign the navbar a new style
+      $('#navbar').addClass('scrolled-navbar');
+
+      // Change the color of all navbar text and icons
+      $('.navbar-link').css({'color':'black'});
+      $('.navbar-social').css({'filter': 'brightness(0)'})
+    }
+
+    // If the scroll has been reduced
+    else if ($(this).scrollTop() < 50) {
+      // Remove the style
+      $('#navbar').removeClass('scrolled-navbar');
+
+      // Change the color of all navbar text and icons
+      $('.navbar-link').css({'color':'white'});
+      $('.navbar-social').css({'filter': 'brightness(0) invert(1)'})
+    }
+  });
+}
